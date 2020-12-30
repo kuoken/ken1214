bf5995f905833247b15d3e5fc315c6c69a79894a
ken <kuoken16@gmail.com>
2020-12-30 22:29:53 +0800

    update website

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -13,7 +13,9 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('歡迎光臨 郭春收的網站') }}
+                    </h2>
                     </x-jet-nav-link>
                 </div>
             </div>
