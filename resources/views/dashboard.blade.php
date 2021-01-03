712510ad46dcfbec97fa1a1b246899b9c8edd3ab
ken <kuoken16@gmail.com>
2021-01-03 15:41:45 +0800

    website update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,27 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        <br>
-        <a class="ml-4 text-sm:items-center text-gray-700 underline" href="/">首頁 <span class="sr-only"></span></a>
-        <a class="ml-4 text-sm:items-center text-gray-700 underline" href="/playlist/">播放清單</a>
-        <a class="ml-4 text-sm:items-center text-gray-700 underline" href="/lucky/">樂透明牌</a>
-        <a href="https://google.com" class="ml-4 underline">Google  </a>
-    </br>
-        </h2>
-    </x-slot>
-
+<html lang="en">
+  <head>
+        <title>選課系統</title>
+       
+  </head>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            
+            <H2> 課程表 </H2>
+<table border="0" width="100%">
+  <tr bgcolor='#FFCCFF'>
+    <td style="width="20%"" align='center' valign="middle" >課程編號</td>
+    <td style="width="20%"" align='center' valign="middle">課程名稱</td>
+    <td style="width="20%"" align='center' valign="middle">課程時間</td> 
+    <td style="width="10%"" align='center' valign="middle"> 修別</td> 
+    <td style="width="20%"" align='center' valign="middle">授課老師</td>
+    <td style="width="10%"" align='center' valign="middle">課程資訊</td>
    
-             
-                
+
+
+  </tr>
+  
             </div>
         </div>
     </div>
-</x-app-layout>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,15 +51,19 @@
           
         <x-guest-layout>
     <x-jet-authentication-card>
-    <h1> 郭春收的期末作業網站</h1>
-            <br>
-           
-            <div class="flex items-center">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                    <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Laravel Documentation</a></div>
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-lg underline">Register</a>
-            </div>    
-            </br>
+    
+ <h2 class="font-semibold text-xl text-gray-800 leading-tight"> 郭春收的期末作業-選課系統</h2>
+     
+     <br>
+     <div class="ml-4 text-sm:items-center text-gray-700"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Laravel Documentation</a></div>
+     <a href="{{ route('register') }}" class="ml-4 text-sm:items-center text-gray-700 underline">Register</a>
+     <a class="ml-4 text-sm:items-center text-gray-700 underline" href="/playlist/">播放清單</a>
+     <a class="ml-4 text-sm:items-center text-gray-700 underline" href="/lucky/">樂透明牌</a>
+     <a href="https://google.com" class="ml-4 underline">Google  </a>
+     <br>
+     <br>
+     
+    
         <x-slot name="logo">
          
         </x-slot>
